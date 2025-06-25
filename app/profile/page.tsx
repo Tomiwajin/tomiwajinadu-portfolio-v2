9c34831e9799020fa992b63d4f1a80b7ae4fa5c6
Tomiwajin <67241225+Tomiwajin@users.noreply.github.com>
2025-06-25 18:56:32 -0400

    Added Home page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,17 @@
+"use client";
 import React from "react";
+import { useViewer } from "@/components/Context/ViewerProvider";
+import RecruiterView from "./RecruiterView";
 
 const page = () => {
-  return;
-  <main>
-    <section></section>
-  </main>;
+  const { viewer } = useViewer();
+  return (
+    <div className="pt-20 px-4 md:pt-10">
+      {viewer === "developer" && <RecruiterView />}
+      {viewer === "recruiter" && <RecruiterView />}
+      {viewer === "stalker" && <RecruiterView />}
+    </div>
+  );
 };
 
 export default page;
