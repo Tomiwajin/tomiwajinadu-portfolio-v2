66051c46d8091ffd1653865fa10b8b94bd9990ce
Tomiwajin <67241225+Tomiwajin@users.noreply.github.com>
2025-07-01 19:36:45 -0400

    Added timeline to professional

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,11 +2,30 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="px-4 md:px-100 flex flex-col gap-20 text-xl">
+    <div className="md:pl-70 flex flex-col gap-20 py-12 md:py-0 ">
       <div className="flex flex-col">
-        <div className="flex flex-row">
-          <div className="px-4 py-2 rounded-md border-2 border-theme bg-red-500">
+        <div className="flex flex-row gap-6">
+          <div className="hidden md:block px-10 py-2 rounded-md border-2 border-theme">
+            All
+          </div>
+          <div className="hidden md:block px-10 py-2 rounded-md border-2 border-theme">
             webDev
+          </div>
+          <div className="hidden md:block px-10 py-2 rounded-md border-2 border-theme">
+            MobileDev
+          </div>
+          <div>
+            <input
+              className="px-2 md:px-10 py-2 rounded-md border-2 border-theme focus:outline-none"
+              type="search"
+              placeholder="search"
+            ></input>
+          </div>
+          <div className="hidden md:block px-10 py-2 rounded-md border-2 border-theme">
+            ML
+          </div>
+          <div className="hidden md:block px-10 py-2 rounded-md border-2 border-theme">
+            JavaDev
           </div>
         </div>
       </div>
