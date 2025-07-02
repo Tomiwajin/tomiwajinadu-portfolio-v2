9e7ff3873bf698260ae2e8dec6206e453b629d85
Tomiwajin <67241225+Tomiwajin@users.noreply.github.com>
2025-07-02 18:49:52 -0400

    updated contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div className=""></div>;
+import React from "react";
+import { LuPhoneOutgoing } from "react-icons/lu";
+import { MdEmail } from "react-icons/md";
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import { LiaLinkedin } from "react-icons/lia";
+import { Coffee, CoffeeIcon } from "lucide-react";
+
+const medium = [
+  {
+    icon: MdEmail,
+    title: "Email Me",
+    description: "Oluwatomiwajinadu@gmail.com",
+    link: "mailto:Oluwatomiwajinadu@gmail.com",
+    category: "Gmail",
+  },
+  {
+    icon: LuPhoneOutgoing,
+    title: "Call Me",
+    description: "+1-470-357-3129",
+    link: "tel:+14703573129",
+    category: "US",
+  },
+  {
+    icon: LiaLinkedin,
+    title: "Connect with Me",
+    description: "Tomiwa Jinadu",
+    link: "https://www.linkedin.com/in/oluwatomiwa-jinadu/",
+    category: "Lindin",
+  },
+];
+
+const ContactPage = () => {
+  return (
+    <div className="md:flex flex-col">
+      <div className="pl-6 md:pl-76 flex flex-wrap mt-10 justify-between">
+        <HoverEffect items={medium} />
+      </div>
+      <div className="flex flex-wrap md:text-xl justify-center items-center">
+        I'm always up for a chat or coffee
+        <Coffee className="text-purple-500" />! Feel free to reach out.
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default ContactPage;
