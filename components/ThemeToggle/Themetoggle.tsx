d46ae6b7afa53d4ee901b4cb694dde9be28b8b11
Tomiwajin <67241225+Tomiwajin@users.noreply.github.com>
2025-09-10 21:09:59 -0400

    Dark theme as default

--- components/ThemeToggle/Themetoggle.tsx
+++ components/ThemeToggle/Themetoggle.tsx
@@ -3,16 +3,21 @@ import { useEffect, useState } from "react";
 import { cn } from "@/lib/utilis";
 
 export const ThemeToggle = () => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   useEffect(() => {
     const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "dark") {
-      setIsDarkMode(true);
-      document.documentElement.classList.add("dark");
-    } else {
+
+    if (storedTheme === "light") {
       setIsDarkMode(false);
       document.documentElement.classList.remove("dark");
+    } else {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+      // Set the preference in localStorage if it doesn't exist
+      if (!storedTheme) {
+        localStorage.setItem("theme", "dark");
+      }
     }
   }, []);
 
